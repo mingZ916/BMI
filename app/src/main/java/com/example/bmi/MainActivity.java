66e70ced2352770cd822dac2458c416b76c39545
mingZ916 <1371314711@qq.com>
2020-03-24 08:42:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bmi;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+    }
+}
